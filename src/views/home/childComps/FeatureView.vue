26774b18a19a9a0ba84013f02d1997d9193cd3a5
fanchangrui <1293707902@qq.com>
2021-08-18 23:19:29 +0800

    滚动

---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="feature">
+  <a href="https://act.mogujie.com/zzlx67"></a>
+  <img src="~assets/img/home/recommend_bg.jpg" alt="">
+</div>
+</template>
+
+<script>
+export default {
+  name: "FeatureView"
+}
+</script>
+
+<style scoped>
+  .feature img{
+    width: 100%;
+  }
+</style>
+
+<!--
+<template>
+  <div class="feature">
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeatureView"
+}
+</script>
+
+<style scoped>
+.feature img {
+  width: 100%;
+}
+</style>
+-->
+
